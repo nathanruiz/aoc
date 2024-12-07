a1461ff44518773bf4d9bddad861fb92c1beeade
Nathan Ruiz <nathan.ruiz@inlife.org.au>
2024-12-07 22:35:55 +1100

    Add day 7 part 2

--- 2024/07/main.py
+++ 2024/07/main.py
@@ -2,29 +2,39 @@ import sys
 from itertools import product
 
 def main():
-    lines = [line.strip() for line in sys.stdin.readlines()]
-
-    result = 0
-    for line in lines:
-        expected, inputs = line.split(": ")
+    lines = []
+    for line in sys.stdin:
+        expected, inputs = line.strip().split(": ")
         expected = int(expected)
         inputs = [int(value) for value in inputs.split()]
-        if is_valid_equation(expected, inputs, ["+", "*"]):
-            result += expected
+        lines.append((expected, inputs))
 
+    result = sum(
+        expected
+        for expected, inputs in lines
+        if is_valid_equation(expected, inputs, ["+", "*"])
+    )
     print(f"[Part one] Calibration results {result}")
 
+    result = sum(
+        expected
+        for expected, inputs in lines
+        if is_valid_equation(expected, inputs, ["+", "*", "||"])
+    )
+    print(f"[Part two] Calibration results {result}")
 
 def is_valid_equation(expected: int, inputs: list[str], operators: list[str]) -> bool:
     for operators in product(operators, repeat=len(inputs) - 1):
         values = list(reversed(inputs))
         for operator in operators:
-            b = values.pop()
             a = values.pop()
+            b = values.pop()
             if operator == "+":
                 values.append(a + b)
             elif operator == "*":
                 values.append(a * b)
+            elif operator == "||":
+                values.append(int(str(a) + str(b)))
 
         if values[0] == expected:
             return True
