256143cea3adaccbb1bcc820af34be710278dbed
Nathan Ruiz <nathan.ruiz@inlife.org.au>
2024-12-08 22:43:09 +1100

    Add day 8 part 2

--- 2024/08/main.py
+++ 2024/08/main.py
@@ -1,12 +1,15 @@
 from dataclasses import dataclass
 from collections import defaultdict
-from itertools import permutations
+from itertools import count, permutations
 import sys
 
 @dataclass(frozen=True)
 class Vector:
     dx: int
     dy: int
+
+    def __mul__(self, value: int) -> "Vector":
+        return Vector(self.dx * value, self.dy * value)
 
 @dataclass(frozen=True)
 class Position:
@@ -26,7 +29,7 @@ class TowerMap:
         self.grid = grid
         self.bounds = (len(grid[0]), len(grid))
 
-    def is_in_bounds(self, position):
+    def is_in_bounds(self, position: Position) -> bool:
         mx, my = self.bounds
         return position.x >= 0 and position.x < mx \
             and position.y >= 0 and position.y < my
@@ -51,11 +54,27 @@ class TowerMap:
             if self.is_in_bounds(antinode)
         }
 
+    def get_antinodes_with_resonant_harmonics(self) -> set[Position]:
+        antinodes = set()
+        for char, positions in self.get_towers().items():
+            for a, b in permutations(positions, 2):
+                for delta, start in [(a - b, a), (b - a, b)]:
+                    for i in count():
+                        pos = start + (delta * i)
+                        if not self.is_in_bounds(pos):
+                            break
+                        antinodes.add(pos)
+        return antinodes
+
 def main():
     grid = [line.strip() for line in sys.stdin.readlines()]
     tower_map = TowerMap(grid)
     antinodes = tower_map.get_antinodes()
     print(f"[Part one] Number of antinodes {len(antinodes)}")
+
+    antinodes = tower_map.get_antinodes_with_resonant_harmonics()
+    print(f"[Part two] Number of antinodes {len(antinodes)}")
+
 
 if __name__ == "__main__":
     main()
