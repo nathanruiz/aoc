60dc9479e26fee69e64beabf57c99fa338b25198
Nathan Ruiz <nathan.ruiz@inlife.org.au>
2024-12-15 16:50:26 +1100

    Add caching into day 11 solution for part 2

--- 2024/11/main.py
+++ 2024/11/main.py
@@ -1,32 +1,45 @@
 import sys
 
-def process_stone(stone: int) -> list[int]:
+def count_stones(iteration: int, stone: int, cache: dict[tuple[int, int], int]) -> int:
+    if iteration == 0:
+        return 1
+
+    result = cache.get((iteration, stone))
+    if result is not None:
+        return result
+
+    result = 0
     size = len(str(stone))
     if stone == 0:
-        return [1]
+        result = count_stones(iteration - 1, 1, cache)
     elif size % 2 == 0:
         mid = int(size / 2)
-        return [
-            int(str(stone)[:mid]),
-            int(str(stone)[mid:]),
-        ]
+        result += count_stones(iteration - 1, int(str(stone)[:mid]), cache)
+        result += count_stones(iteration - 1, int(str(stone)[mid:]), cache)
     else:
-        return [stone * 2024]
+        result = count_stones(iteration - 1, stone * 2024, cache)
+
+    cache[(iteration, stone)] = result
+    return result
 
 def main():
     stones = [
         int(stone)
         for stone in sys.stdin.readline().strip().split()
     ]
+    cache = {}
 
-    for _ in range(25):
-        stones = [
-            new_stone
-            for stone in stones
-            for new_stone in process_stone(stone)
-        ]
+    count = sum(
+        count_stones(25, stone, cache)
+        for stone in stones
+    )
+    print(f"[Part one] {count} stones")
 
-    print(f"[Part one] {len(stones)} stones")
+    count = sum(
+        count_stones(75, stone, cache)
+        for stone in stones
+    )
+    print(f"[Part two] {count} stones")
 
 if __name__ == "__main__":
     main()
