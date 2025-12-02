092dccbf4703e99adfc5f384d1a0511b86509976
Nathan Ruiz <nathan.ruiz@protonmail.com>
2025-12-02 11:55:15 +0000

    Add part 2 for day 2

--- 2025/02/main.py
+++ 2025/02/main.py
@@ -1,3 +1,4 @@
+import itertools
 import sys
 from typing import Generator
 
@@ -6,7 +7,7 @@ class IDRange:
         self.start = start
         self.end = end
 
-    def invalid_values(self) -> Generator[int, None, None]:
+    def get_pairs(self) -> Generator[int, None, None]:
         for i in range(self.start, self.end + 1):
             i_str = str(i)
             if len(i_str) % 2 != 0:
@@ -16,19 +17,45 @@ class IDRange:
             if i_str[pivot:] == i_str[:pivot]:
                 yield i
 
-def part1(ranges: list[str]):
-    invalid_sum = 0
-    for id_range_str in ranges:
-        start, end = id_range_str.split("-")
-        id_range = IDRange(int(start), int(end))
-        invalid_sum += sum(id_range.invalid_values())
+    def get_repeated(self) -> Generator[int, None, None]:
+        for i in range(self.start, self.end + 1):
+            i_str = str(i)
 
+            for divisor in range(1, len(i_str)):
+                if len(i_str) % divisor != 0:
+                    continue
+
+                if self.is_repeated(i_str, divisor):
+                    yield i
+                    break
+
+    @classmethod
+    def is_repeated(cls, value: str, divisor: int) -> bool:
+        chunks = list(itertools.batched(value, divisor))
+        for chunk in chunks[1:]:
+            if chunks[0] != chunk:
+                return False
+        return True
+
+
+def part1(ranges: list[str]):
+    invalid_sum = sum(sum(id_range.get_pairs()) for id_range in ranges)
     print(f"[Part One] Invalid sum {invalid_sum}")
 
 
+def part2(ranges: list[str]):
+    invalid_sum = sum(sum(id_range.get_repeated()) for id_range in ranges)
+    print(f"[Part Two] Invalid sum {invalid_sum}")
+
+
 def main():
-    ranges = sys.stdin.read().split(",")
+    ranges = []
+    for id_range_str in sys.stdin.read().split(","):
+        start, end = id_range_str.split("-")
+        ranges.append(IDRange(int(start), int(end)))
+
     part1(ranges)
+    part2(ranges)
 
 if __name__ == "__main__":
     main()
