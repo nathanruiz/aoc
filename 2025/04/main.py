68ce7ab41321586a80a1470e9acc665f6d87d7a3
Nathan Ruiz <nathan.ruiz@protonmail.com>
2025-12-05 13:01:02 +0000

    Add day 4 part 1

---
+++ 2025/04/main.py
@@ -0,0 +1,79 @@
+from dataclasses import dataclass
+import sys
+
+@dataclass
+class Bounds:
+    width: int
+    height: int
+
+    def __contains__(self, location: "Location") -> bool:
+        return (
+            location.x >= 0 and location.y >= 0 and
+            location.x < self.width and location.y < self.height
+        )
+
+    def __iter__(self):
+        for x in range(self.width):
+            for y in range(self.height):
+                yield Location(x, y)
+
+
+@dataclass
+class Location:
+    x: int
+    y: int
+
+    def __add__(self, location: "Location") -> None:
+        return Location(location.x + self.x, location.y + self.y)
+
+
+class Grid:
+    rolls: list[list[bool]]
+    bounds: Bounds
+
+    def __init__(self, lines: list[str]) -> None:
+        self.rolls = [
+            [cell == "@" for cell in line.strip()]
+            for line in lines
+        ]
+        self.bounds = Bounds(
+            width=len(self.rolls[0]),
+            height=len(self.rolls),
+        )
+
+    def is_roll(self, location: Location) -> bool:
+        if location not in self.bounds:
+            return False
+
+        return self.rolls[location.y][location.x]
+
+    def get_accessible_rolls(self) -> list[Location]:
+        locations = []
+        for location in self.bounds:
+            if self.is_roll(location) and self.is_accessible(location):
+                locations.append(location)
+
+        return locations
+
+    def is_accessible(self, location: Location) -> bool:
+        roll_count = 0
+        for x in [-1, 0, 1]:
+            for y in [-1, 0, 1]:
+                neighbor = location + Location(x, y)
+                if self.is_roll(neighbor):
+                    roll_count += 1
+
+        if roll_count <= 4:
+            return True
+
+
+def main():
+    lines = sys.stdin.readlines()
+    grid = Grid(lines)
+    locations = grid.get_accessible_rolls()
+
+    print(f"[Part one] Accessible rolls {len(locations)}")
+
+
+if __name__ == "__main__":
+    main()
