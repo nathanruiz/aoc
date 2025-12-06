bd0af35275ecea6fe3813b9b98efc1b9560a9c94
Nathan Ruiz <nathan.ruiz@protonmail.com>
2025-12-06 11:48:08 +0000

    Add day 5 part 2

--- 2025/05/main.py
+++ 2025/05/main.py
@@ -1,13 +1,36 @@
 import sys
+from dataclasses import dataclass
+
+@dataclass(frozen=True)
+class Range:
+    start: int
+    end: int
+
+    def __contains__(self, ingredient_id: int) -> bool:
+        return self.start <= ingredient_id and ingredient_id <= self.end
+
+    def overlaps(self, other: "Range") -> bool:
+        return self.start <= other.end and other.start <= self.end
+
+    def merge(self, other: "Range") -> "Range":
+        if not self.overlaps(other):
+            raise ValueError("Unable to merge non-contiguous ranges")
+
+        start = min(self.start, other.start)
+        end = max(self.end, other.end)
+        return Range(start, end)
+
+    def __len__(self) -> int:
+        return self.end - self.start + 1
 
 
 def main():
-    fresh_ranges: list[tuple[int, int]] = []
+    fresh_ranges: list[Range] = []
     for line in sys.stdin:
         if line.strip() == "":
             break
         start, end = line.strip().split("-")
-        fresh_ranges.append((int(start), int(end)))
+        fresh_ranges.append(Range(int(start), int(end)))
 
     ingredients: list[int] = []
     for line in sys.stdin:
@@ -15,13 +38,24 @@ def main():
 
     fresh_count = 0
     for ingredient in ingredients:
-        for start, end in fresh_ranges:
-            if ingredient in range(start, end + 1):
+        for fresh_range in fresh_ranges:
+            if ingredient in fresh_range:
                 fresh_count += 1
                 break
 
     print(f"[Part one] Number of fresh ingredients {fresh_count}")
 
+    merged_ranges = set()
+    for fresh_range in fresh_ranges:
+        for next_range in list(merged_ranges):
+            if fresh_range.overlaps(next_range):
+                merged_ranges.remove(next_range)
+                fresh_range = fresh_range.merge(next_range)
+
+        merged_ranges.add(fresh_range)
+
+    fresh_ids = sum(len(fresh_range) for fresh_range in merged_ranges)
+    print(f"[Part two] Number of fresh IDs {fresh_ids}")
 
 if __name__ == "__main__":
     main()
